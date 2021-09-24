99d3691d6a0d09186e454a5c2a88eca032f5306c
Alegherix <psao69@gmail.com>
2021-09-24 11:38:21 +0200

    added plugin

---
+++ html/wp-content/plugins/alegherixPlugins/test.js
@@ -0,0 +1,8 @@
+wp.blocks.registerBlockType('alegherx/test', {
+  title: 'Test Title',
+  Icon: 'smiley',
+  category: 'common',
+  edit: function () {},
+
+  save: function () {},
+});
---
+++ html/wp-content/plugins/hugoPlugin/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'bb4cb86e91b8bbf7d3d6aa429b0d3b7f');
